7a99489d3b36b1c2c40895b0dfa6c3f40752feab
javierllopez <javierllopez@hotmail.com>
2025-08-27 10:11:38 -0300

    Modificación de perfil y cambio de password

--- routes/login.js
+++ routes/login.js
@@ -40,11 +40,11 @@ router.post('/', noLogueado, async (req, res) => {
 
         await pool.query('UPDATE usuarios SET Intentos = 0, BloqueadoHasta = NULL WHERE usuario = ?', [usuario]);
         req.session.idUsuario = user[0].Id; // Store user id in session
-        req.session.usuario = usuarioData.Usuario; // Store username in session
-        req.session.nivelUsuario = usuarioData.Nivel; // Store user level in session
+        req.session.usuario = user[0].Usuario; // Store username in session
+        req.session.nivelUsuario = user[0].Nivel; // Store user level in session
 
         if (usuarioData.primerAcceso) {
-            return render(req,res,'cambiarClave', {Id: user[0].Id, CorreoElectronico: user[0].CorreoElectronico, primeraVez: true})  // Si es el primer acceso, redirigir a cambiar clave
+            return render(req,res,'cambiarClave', {Id: user[0].Id, UsuarioActual: user[0].Usuario, CorreoElectronico: user[0].CorreoElectronico, primeraVez: true})  // Si es el primer acceso, redirigir a cambiar clave
         } else {
             return res.redirect('/'); // Redirect to a dashboard or home page after successful login
         }
@@ -54,27 +54,58 @@ router.post('/', noLogueado, async (req, res) => {
     }
 });
 
-router.get('/cambiarClave', logueado, (req, res) => {
-    return render(req, res, 'cambiarClave');
+router.get('/cambiarClave', logueado,  async (req, res) => {
+    const [user] = await pool.query('SELECT * FROM usuarios WHERE Id = ?', [req.session.idUsuario]);
+    return render(req, res, 'cambiarClave',{Id: req.session.idUsuario, UsuarioActual: req.session.usuario, CorreoElectronico: user[0].CorreoElectronico});
 });
 
 router.post('/cambiarClave', logueado, async (req, res) => {
-    const { Id, nuevoUsuario, correoElectronico, newPassword, confirmPassword } = req.body;
+    const { Id, nuevoUsuario, correoElectronico, claveActual, newPassword, confirmPassword, primeraVez } = req.body;
     const sqlUsuarioExistente = 'SELECT * FROM usuarios WHERE Usuario = ? AND Id != ?';
-    if (newPassword !== confirmPassword) {
-        return render(req, res, 'cambiarClave', { Mensaje: { title: 'Atención', text: 'Las contraseñas no coinciden', icon: 'error' } });
-    }
-    try {
-        const [usuarioExistente] = await pool.query(sqlUsuarioExistente, [nuevoUsuario, Id]);
-        if (usuarioExistente.length > 0) {
-            return render(req, res, 'cambiarClave', { Mensaje: { title: 'Atención', text: 'El nombre de usuario ya existe', icon: 'error' } });
+    const sqlUsuarioAEditar = 'SELECT * FROM usuarios WHERE Id = ?';
+    if (!primeraVez) {
+        if (newPassword !== confirmPassword) {
+            return render(req, res, 'cambiarClave', { Mensaje: { title: 'Atención', text: 'Las contraseñas no coinciden', icon: 'error' }, primeraVez: primeraVez, UsuarioActual: nuevoUsuario, CorreoElectronico: correoElectronico });
         }
-        const hashedPassword = await bcrypt.hash(newPassword, 10);
-        await pool.query('UPDATE usuarios SET Usuario = ?, Clave = ?, CorreoElectronico = ?, primerAcceso = 0 WHERE Id = ?', [nuevoUsuario, hashedPassword, correoElectronico, Id]);
-        return res.redirect('/');
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send('Error en el servidor');
+        try {
+            // Validar que claveActual sea la clave correcta
+            const [usuarioAEditar] = await pool.query(sqlUsuarioAEditar, [req.session.idUsuario]);
+            if (usuarioAEditar.length === 0) {
+                return render(req, res, 'cambiarClave', { Mensaje: { title: 'Atención', text: 'Usuario no encontrado', icon: 'error' }, primeraVez: primeraVez, UsuarioActual: nuevoUsuario, CorreoElectronico: correoElectronico });
+            }
+            const claveCorrecta = await bcrypt.compare(claveActual, usuarioAEditar[0].Clave);
+            if (!claveCorrecta) {
+                return render(req, res, 'cambiarClave', { Mensaje: { title: 'Atención', text: 'La contraseña actual es incorrecta', icon: 'error' }, primeraVez: primeraVez, UsuarioActual: nuevoUsuario, CorreoElectronico: correoElectronico });
+            }
+            const [usuarioExistente] = await pool.query(sqlUsuarioExistente, [nuevoUsuario, Id]);
+            if (usuarioExistente.length > 0) {
+                return render(req, res, 'cambiarClave', { Mensaje: { title: 'Atención', text: 'El nombre de usuario ya existe', icon: 'error' }, primeraVez: primeraVez, UsuarioActual: nuevoUsuario, CorreoElectronico: correoElectronico });
+            }
+            const hashedPassword = await bcrypt.hash(newPassword, 10);
+            await pool.query('UPDATE usuarios SET Usuario = ?, Clave = ?, CorreoElectronico = ?, primerAcceso = 0 WHERE Id = ?', [nuevoUsuario, hashedPassword, correoElectronico, Id]);
+            req.session.usuario = nuevoUsuario; // Update session username            
+            return res.redirect('/');
+        } catch (error) {
+            console.error(error);
+            return res.status(500).send('Error en el servidor');
+        }}
+    else {
+        if (newPassword !== confirmPassword) {
+            return render(req, res, 'cambiarClave', { Mensaje: { title: 'Atención', text: 'Las contraseñas no coinciden', icon: 'error' }, primeraVez: primeraVez, UsuarioActual: nuevoUsuario, CorreoElectronico: correoElectronico});
+        }
+        try {
+            const [usuarioExistente] = await pool.query(sqlUsuarioExistente, [nuevoUsuario, Id]);
+            if (usuarioExistente.length > 0) {
+                return render(req, res, 'cambiarClave', { Mensaje: { title: 'Atención', text: 'El nombre de usuario ya existe', icon: 'error' }, primeraVez: primeraVez, UsuarioActual: nuevoUsuario, CorreoElectronico: correoElectronico});
+            }
+            const hashedPassword = await bcrypt.hash(newPassword, 10);
+            await pool.query('UPDATE usuarios SET Usuario = ?, Clave = ?, CorreoElectronico = ?, primerAcceso = 0 WHERE Id = ?', [nuevoUsuario, hashedPassword, correoElectronico, Id]);
+            req.session.usuario = nuevoUsuario; // Update session username
+            return res.redirect('/');
+        } catch (error) {
+            console.error(error);
+            return res.status(500).send('Error en el servidor');
+        }
     }
 });
 
